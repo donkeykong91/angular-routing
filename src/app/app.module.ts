be6182b912eb0f13285d71b1f6ed46a027f925b5
donkeykong91 <darylnator@gmail.com>
2020-04-10 09:08:10 -0700

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,18 @@ import {ResetComponent} from "./reset-password/reset-password.component";
 import {OutletComponent} from "./outlet/outlet.component";
 import {NotFoundComponent} from "./404/404.component";
 
+const appRoutes: Routes = [
+  {path: '', component: OutletComponent},
+  {path: 'login', component: LoginComponent}, 
+  {path: 'reset-password', component: ResetComponent}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
+    OutletComponent,
+    LoginComponent,
+    ResetComponent
   ],
   imports: [
     BrowserModule,
