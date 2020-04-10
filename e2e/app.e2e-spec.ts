8b2b3bedc9e02b33c9e3bb7fd0bdd8f87bb390bf
donkeykong91 <darylnator@gmail.com>
2020-04-10 10:39:06 -0700

    new files to work with

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('parameters App', () => {
+describe('modualize App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import {ProductServices} from './services/product.service';
-import {Router} from '@angular/router';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
- products: any;
-
-  constructor(private router: Router, 
-              private productServices: ProductServices) { }
-
-  ngOnInit() {
-    this.products = this.productServices.productList();
-  }
-
-  show(id) {
-    this.router.navigate(['details', id, 'fred']);
-  }
-
+export class AppComponent {
 }
